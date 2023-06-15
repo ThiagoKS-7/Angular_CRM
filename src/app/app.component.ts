4c54484f21f92b9637bc0d4b3695c9c3da5ffa38
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2023-06-15 16:31:24 -0300

    Initial config - dashboard layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'crm-app';
+
+  constructor(private modalService: NgbModal) {
+  }
+  waitingForService = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+  inAttendence = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+  proposalMade = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+  notCompleted = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+  sold = [];
+
+  public open(modal: any): void {
+    this.modalService.open(modal);
+  }
+
+  public openNewTab(url:string) {
+    window.open(url, "_blank");
+  }
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,15 @@
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {NgFor} from '@angular/common';
+import {
+  CdkDrag,
+  CdkDropList,
+  CdkDropListGroup,
+} from '@angular/cdk/drag-drop';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +20,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    NgbModule,
+    AppRoutingModule,
+    CdkDropListGroup, 
+    MatButtonModule,
+    MatTooltipModule,
+    MatIconModule,
+    CdkDropList, 
+    NgFor, 
+    CdkDrag
   ],
   providers: [],
   bootstrap: [AppComponent]
